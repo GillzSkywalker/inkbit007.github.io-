dd251ad38e7644b8f9c5f01aff2758f723e3a0a6
Gillian Alindayu <itzagentskywalker@gmail.com>
2026-01-21 16:01:45 +0400

    Hopefully it works

--- passport-config.js
+++ passport-config.js
@@ -1,6 +1,36 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 const User = require('./models/user');
+
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      // Allow login with either email or username (name)
+      let user = await User.findOne({ email: username });
+      if (!user) {
+        user = await User.findOne({ name: username });
+      }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+
+      if (!user.password) {
+        return done(null, false, { message: 'Please log in with Google.' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
